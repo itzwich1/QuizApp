b8508def2ff432dc6169c5cc5caa3fe2384b2c46
Felix <fx.wich@icloud.com>
2025-12-03 18:02:44 +0100

    AuthService hinzugefuegt zum registrieren und anmelden von benutzern

---
+++ QuizApp/Services/AuthService.swift
@@ -0,0 +1,45 @@
+//
+//  AuthServices.swift
+//  QuizApp
+//
+//  Created by Felix Wich on 03.12.25.
+//
+
+import SwiftData
+import Foundation
+import Combine
+
+class AuthService: ObservableObject {
+    
+    @Published var isLoggedIn: Bool = false
+    
+    func registerUser(username: String, passwordText: String, context: ModelContext) -> Bool {
+            
+            // Prüfen, ob Benutzername bereits existiert (optional, aber gut)
+            /*if getUser(username: username, context: context) != nil {
+                print("Fehler: Benutzername ist bereits vergeben.")
+                return false
+            }*/
+            
+            // 1. Passwort hashen (sicherstellen, dass Klartext nie gespeichert wird)
+            let safeHash = ""
+            
+            // 2. Neues Benutzer-Modell erstellen
+            let newUser = UserModel(username: username, passwordHash: safeHash)
+            
+            // 3. Im lokalen SwiftData-Speicher ablegen
+            context.insert(newUser)
+            
+            do {
+                try context.save()
+                print("Benutzer \(username) erfolgreich in SwiftData registriert.")
+                return true
+            } catch {
+                print("FEHLER beim Speichern des Benutzers: \(error)")
+                return false
+            }
+        }
+    
+    
+    
+}
